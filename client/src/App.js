0b1fe4196c19a48ae4fc6a384a94686682bddd72
Aditya-Saini <aju180601@gmail.com>
2020-11-14 14:23:21 +0530

    Adding compnent list and fade effects for delete action

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import AppNavbar from './components/AppNavbar'
+import ShoppingList from './components/ShoppingList'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <AppNavbar/>
+        <ShoppingList/>
     </div>
   );
 }
