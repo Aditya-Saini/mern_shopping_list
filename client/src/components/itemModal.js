4870ba815fc3898b125c6bf31e6b68f000765749
Aditya-Saini <aju180601@gmail.com>
2020-12-15 23:35:07 +0530

    init for add button

---
+++ client/src/components/itemModal.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {
+    Button,
+    Modal,
+    ModalHeader,
+    MOdalBody,
+    Form,
+    FormGroup,
+    Label,
+    Input
+} from 'reactstrap';
+import {connect} from 'react-redux';
+import {addItem} from '../actions/itemActions';
+
+class ItemModal extends Component {
+
+}
+
+export default connect()(ItemModal);
