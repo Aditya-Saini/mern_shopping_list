0d8a187c56f4f7d5c19363b0acc06ef0d4a873f5
Aditya-Saini <aju180601@gmail.com>
2020-12-16 00:02:11 +0530

    adding add item functionalities

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -33,6 +33,11 @@ export default (state = initialState, action) => {
             return {
                 ...state,
                 items: state.items.filter(item => item.id !== action.payload)
+            };
+        case ADD_ITEM:
+            return {
+                ...state,
+                items: [action.payload, ...state.items]
             }
         default:
             return state;
