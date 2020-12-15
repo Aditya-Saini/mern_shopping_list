e8ac855dae5835f83faca2b878302a9d365d2d22
Aditya-Saini <aju180601@gmail.com>
2020-12-15 22:56:08 +0530

    redux implement of get items static

---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,35 @@
+import {v4 as uuid} from 'uuid';
+import {GET_ITEMS, ADD_ITEM, DELETE_ITEM} from '../actions/types';
+
+const initialState={
+    items: [
+        {
+            id:uuid(),
+            name: 'Eggs' 
+        },
+        {
+            id:uuid(),
+            name: 'Milk' 
+        },
+        {
+            id:uuid(),
+            name: 'water' 
+        },
+        {
+            id:uuid(),
+            name: 'steak' 
+        }
+    ]
+}
+
+
+export default (state = initialState, action) => {
+    switch(action.type) {
+        case GET_ITEMS:
+            return {
+                ...state
+            }
+        default:
+            return state;
+    }
+}
--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,7 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+//import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
 
 const initialState = {}; //representing our initial state.
 
