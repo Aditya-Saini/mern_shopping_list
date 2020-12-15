32bbafab7ac50d11bc2d81682fc1fdbda8d49e87
Aditya-Saini <aju180601@gmail.com>
2020-12-16 01:22:50 +0530

    setting up serverjs when the app is pushed into heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express'),
       mongoose=require('mongoose'),
-      bodyParser=require('body-parser');
+      bodyParser=require('body-parser'),
+      path=require('path');
 
 const items= require('./routes/api/item')
 
@@ -20,6 +21,16 @@ mongoose
 
 //use ROutes
 app.use('/api/items', items);
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const port = process.env.PORT || 5000
 
