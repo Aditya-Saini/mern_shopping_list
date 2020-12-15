1daad889ccb131c177df1972244748d4de48e70e
Aditya-Saini <aju180601@gmail.com>
2020-12-15 20:16:33 +0530

    template of storejs

--- client/src/store.js
+++ client/src/store.js
@@ -1,1 +1,14 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {}; //representing our initial state.
+
+const middleware = [thunk]; // variable for all the middleware.
+
+const store = createStore(rootReducer, initialState, compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));//store variable createstore function takes 3 things rootreducer, initialstate and middleware since we are using redux tools we will be wrapping it in compose.
+
+export default store;
