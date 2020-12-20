ba78670d6b4f9b07413ffc627495fc357ec42d6d
Aditya-Saini <aju180601@gmail.com>
2020-12-20 18:28:16 +0530

    init for error & auth reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 //meeting place for all the reducers
 import {combineReducers} from 'redux';
 import itemReducer from './itemReducer';
+import itemReducer from './errorReducer';
+import itemReducer from './authReducer';
 
 export default combineReducers({
     item: itemReducer
