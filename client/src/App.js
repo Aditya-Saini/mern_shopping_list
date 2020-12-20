4c2558529721fa669a285ec0d269a5b69f94dc54
Aditya-Saini <aju180601@gmail.com>
2020-12-20 19:42:10 +0530

    to continously call the loginuser action

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,27 @@ import './App.css';
 
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/authActions';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <Provider store={store}>
-      <div className="App">
-          <AppNavbar/>
-          <Container>
-            <ItemModal/>
-            <ShoppingList/>
-          </Container>
-      </div>
-    </Provider>
-  );
+class App extends Component {
+  //for continously calling auth
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+            <AppNavbar/>
+            <Container>
+              <ItemModal/>
+              <ShoppingList/>
+            </Container>
+        </div>
+      </Provider>
+    );
+  };
 }
 
 export default App;
