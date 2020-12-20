fa90f246fe32d8fde8e49936b7afc8fef72ba367
Aditya-Saini <aju180601@gmail.com>
2020-12-20 18:29:45 +0530

    exporting error & auth reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -5,5 +5,7 @@ import errorReducer from './errorReducer';
 import authReducer from './authReducer';
 
 export default combineReducers({
-    item: itemReducer
+    item: itemReducer,
+    error: errorReducer,
+    auth: authReducer
 });
