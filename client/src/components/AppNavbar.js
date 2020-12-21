84e4c0674bbde9d1b4fea3a94681502b22483db7
Aditya-Saini <aju180601@gmail.com>
2020-12-21 22:13:35 +0530

    add register modal, remove github link

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -10,6 +10,7 @@ import {
     NavLink,
     Container
 } from 'reactstrap';
+import RegisterModal from './auth/RegisterModal';
 
 class AppNavbar extends Component {
     state={
@@ -32,9 +33,7 @@ class AppNavbar extends Component {
                         <Collapse isOpen={this.state.isOpen} navbar>
                             <Nav className="ml-auto" navbar>
                                 <NavItem>
-                                    <NavLink href="https://github.com/Aditya-Saini">
-                                        Github
-                                    </NavLink>
+                                    <RegisterModal />
                                 </NavItem>
                             </Nav>
                         </Collapse>
