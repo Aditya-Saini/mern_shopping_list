29bfacdf0b7d1c8727846a392ccf579dc0d83151
Aditya-Saini <aju180601@gmail.com>
2020-12-23 15:53:12 +0530

    add and delete will only be accessible once you log in

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -7,6 +7,12 @@ import PropTypes from 'prop-types';
 
 class ShoppingList extends Component {
 
+    static propTypes = {
+        getItems: PropTypes.func.isRequired,
+        item: PropTypes.object.isRequired,
+        isAuthenticated: PropTypes.bool
+    }
+
     componentDidMount() {
         this.props.getItems();
     }
@@ -23,13 +29,13 @@ class ShoppingList extends Component {
                         {items.map(({_id,name})=> (
                             <CSSTransition Key={_id} timeout={500} classNames="fade">
                                 <ListGroupItem>
-                                    <Button
+                                    { this.props.isAuthenticated ?  <Button
                                         className="remove-btn"
                                         color="danger"
                                         size="sm"
                                         onClick= {this.onDeleteClick.bind(this, _id)}>
                                         &times;
-                                    </Button>
+                                    </Button> : null}
                                     {name}
                                 </ListGroupItem>
                             </CSSTransition>
@@ -42,13 +48,9 @@ class ShoppingList extends Component {
     }
 }
 
-ShoppingList.propTypes = {
-    getItems: PropTypes.func.isRequired,
-    item: PropTypes.object.isRequired
-}
-
 const mapStateToProps = (state) => ({
-    item: state.item
+    item: state.item,
+    isAuthenticated: state.auth.isAuthenticated
 });
 
 export default connect(mapStateToProps, { getItems, deleteItem })(ShoppingList);
